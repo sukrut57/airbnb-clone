f6bfc9601b27bd055e71cd4a7407bbadcd52c95e
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2025-08-11 16:33:22 -0400

    navbar completed

--- airbnb-clone-frontend/src/app/layout/navbar/navbar.component.ts
+++ airbnb-clone-frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {ToolbarModule} from 'primeng/toolbar';
-import {Button, ButtonDirective} from 'primeng/button';
+import {Button, ButtonDirective, ButtonModule} from 'primeng/button';
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
@@ -9,12 +11,31 @@ import {Button, ButtonDirective} from 'primeng/button';
   imports: [
     FaIconComponent,
     ToolbarModule,
-    Button,
-    ButtonDirective
+    ButtonModule,
+    MenuModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  items!: MenuItem[];
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        items: [
+          { label: 'New', icon: 'pi pi-plus', command: () => alert('New clicked') },
+          { label: 'Open', icon: 'pi pi-folder-open', command: () => alert('Open clicked') }
+        ]
+      },
+      {
+        label: 'Edit',
+        items: [
+          { label: 'Undo', icon: 'pi pi-undo', command: () => alert('Undo clicked') },
+          { label: 'Redo', icon: 'pi pi-redo', command: () => alert('Redo clicked') }
+        ]
+      }
+    ];
+  }
 }
--- airbnb-clone-frontend/src/app/shared/font-awesome-icons.ts
+++ airbnb-clone-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,9 +1,10 @@
 import {IconDefinition} from '@fortawesome/free-regular-svg-icons';
 import {faAirbnb, faGithub} from '@fortawesome/free-brands-svg-icons';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faBars, faSearch} from '@fortawesome/free-solid-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[] = [
   faAirbnb,
   faSearch,
-  faGithub
+  faGithub,
+  faBars
 ];
